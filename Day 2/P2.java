54244c9072dda31013f735fb2ad04ded9cfdd98a
Prathamesh <prathmeshdiwane29@gmail.com>
2024-10-01 23:37:57 +0530

    Day 2

---
+++ Day 2/P2.java
@@ -0,0 +1,10 @@
+class P2{
+	public static void main(String[]args){
+		for(int a=1;a<=20;a++){
+			if(a%2==0){
+			System.out.println(a);
+			}
+
+		}
+	}
+}
---
+++ Day 2/P4.java
@@ -0,0 +1,9 @@
+class P4{
+	public static void main(String[]args){
+		for(int b=1;b<=20;b++){
+		if(b%3==0 && b%7==0){
+		System.out.println(b);
+			}
+		}
+	}
+}
