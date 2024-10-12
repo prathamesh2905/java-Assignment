a6377e786848bc087731b0a49b941f6effed8702
Prathamesh <prathmeshdiwane29@gmail.com>
2024-10-12 23:22:09 +0530

    Day 5

---
+++ Day 5/P2.java
@@ -0,0 +1,12 @@
+class P2{
+	public static void main(String[]args){
+		int a = 20, b = 50, c = 40;
+       		 if(a <= b && a <= c) {
+           	 System.out.println(a + " is the smallest among all numbers");
+       		 } else if(b <= a && b <= c) {
+           	 System.out.println(b + " is the smallest among all numbers");
+      		  } else {
+         	   System.out.println(c + " is the a smallest among all numbers");
+      		  }
+	}
+}
---
+++ Day 5/P5.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String[]args){
+		int a=1;
+		for(int i=0;i<3;i++){
+			for(int j=0;j<2;j++){
+				System.out.print("*"+a);
+				a++;
+			
+				
+				
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
