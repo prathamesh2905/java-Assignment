4c7209fa8ecaffb33504d6904040af30d30fc1d1
Prathamesh <prathmeshdiwane29@gmail.com>
2024-10-13 15:03:55 +0530

    Day 6

---
+++ Day 6/p7.java
@@ -0,0 +1,12 @@
+class p7{
+	public static void main(String args[]){
+		for(int i=1; i<=4; i++){
+			char ch='A';
+			for(int j=1; j<=4; j++){
+				System.out.print(ch+" ");
+				ch++;
+			}
+		System.out.println();
+		}
+	}
+}
