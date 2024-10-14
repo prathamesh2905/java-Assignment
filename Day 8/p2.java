b73de9a40ae3d20e98f7fbaa76d43ae41b262c42
Prathamesh <prathmeshdiwane29@gmail.com>
2024-10-14 22:15:11 +0530

    Day 8

---
+++ Day 8/p2.java
@@ -0,0 +1,15 @@
+class p2{
+	public static void main(String[]args){
+	char ch='A';
+	for(int i=0;i<5;i++){
+		for(int j=0;j<i;j++){
+			System.out.print(ch+" ");
+			ch++;
+			}
+		System.out.println();
+
+		}
+
+	}
+
+}
---
+++ Day 8/p5.java
@@ -0,0 +1,17 @@
+class p5{
+	public static void main(String[]args){
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+		if(i%2==0){
+			System.out.print("* ");
+
+			}else{
+			System.out.print("= ");
+				}
+			}
+		System.out.println();
+		}
+
+	}
+
+}
